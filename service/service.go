657bacf3acebc8f1fa1724d0032904b21780b95a
Foad Momtazi <fmomtazi@nex.global>
2023-07-28 14:31:35 +0100

    added checkForStaleServices

--- service/service.go
+++ service/service.go
@@ -1,14 +1,22 @@
 package service
 
 import (
+	"fmt"
+	"sync"
+
 	m "github.com/foadmom/appGateway/heartBeat"
 )
 
-// the key for the map is service name
-var ServiceCache map[string]ServiceMapElement
+type cacheType struct {
+	// the key for the map is service name
+	cache map[string]ServiceMapElement
+	lock  sync.Mutex
+}
+
+var ServiceCache cacheType = cacheType{}
 
 func init() {
-	ServiceCache = make(map[string]ServiceMapElement)
+	ServiceCache.cache = make(map[string]ServiceMapElement)
 }
 
 type ServiceMapElement struct {
@@ -26,13 +34,56 @@ type ServiceMapElement struct {
 func addNewService(service m.Service) error {
 	var _err error
 	var _list []m.Service = make([]m.Service, 0, 6)
-	var _mapElement ServiceMapElement = ServiceMapElement{_list, -1}
 
 	_list = append(_list, service)
+	var _mapElement ServiceMapElement = ServiceMapElement{_list, -1}
 
-	ServiceCache[service.Name] = _mapElement
+	ServiceCache.cache[service.Name] = _mapElement
 
 	return _err
+}
+
+// ========================================================
+// add another instance to a service list
+// ========================================================
+func (e ServiceMapElement) addSeviceInstance(service m.Service) []m.Service {
+	_list := e.List
+	_list = append(_list, service)
+
+	return _list
+}
+
+// ========================================================
+// add another instance to a service list
+// ========================================================
+func (e ServiceMapElement) updateService(service m.Service) {
+	_list := e.List
+	for _index, _service := range _list {
+		if (_service.Host == service.Host) &&
+			(_service.Port == service.Port) {
+			// this is an existing service there we should update
+			// the same service status
+			_list[_index] = service
+			e.List = _list
+			return
+		}
+	}
+	// we did not find a match for the same host and port
+	// so it must be a new instance of the same service
+	e.List = e.addSeviceInstance(service)
+	ServiceCache.cache[service.Name] = e
+}
+
+// ========================================================
+// every so often we check for stale service entries
+// if the timeStamp is older than a threshold
+// remove the entry. If the service starts again
+// it will register again
+// ========================================================
+func checkForStaleServices() {
+	ServiceCache.lock.Lock()
+	defer ServiceCache.lock.Unlock()
+
 }
 
 // ============================================================================
@@ -41,15 +92,38 @@ func addNewService(service m.Service) error {
 // ========================================================
 // update/add a service
 // ========================================================
-func Update(service m.Service) error {
+func UpdateServiceInfo(service m.Service) error {
 	var _err error
 
+	ServiceCache.lock.Lock()
+	defer ServiceCache.lock.Unlock()
+	_elem, _found := ServiceCache.cache[service.Name]
+	if _found {
+		_elem.updateService(service)
+	} else {
+		addNewService(service)
+	}
 	return _err
+}
+
+func ServiceDiscovery() (m.Service, error) {
+	var _err error
+	var _service m.Service
+
+	ServiceCache.lock.Lock()
+	defer ServiceCache.lock.Unlock()
+
+	return _service, _err
 }
 
 // ============================================================================
 // ============== debug functions
 // ============================================================================
-func PrintCache () {
-	for _, _service 
+func PrintCache() {
+	for _name, _service := range ServiceCache.cache {
+		fmt.Printf("service name=%s  Index=%d\n", _name, _service.Index)
+		for _, _elem := range _service.List {
+			fmt.Printf("    %v\n", _elem)
+		}
+	}
 }
--- service/service_test.go
+++ service/service_test.go
@@ -9,14 +9,35 @@ import (
 
 func Test_addNewService(t *testing.T) {
 	// var _err error
-	var _service_A m.Service = m.Service{Name: "Service-A", LastUpdated: time.Now()}
+	var _service_A m.Service = m.Service{Name: "Service-A", Host: "localHost", Port: 7666, LastUpdated: time.Now()}
 	time.Sleep(1 * time.Second)
-	var _service_B m.Service = m.Service{Name: "Service-B", LastUpdated: time.Now()}
+	var _service_B m.Service = m.Service{Name: "Service-B", Host: "localHost", Port: 7555, LastUpdated: time.Now()}
 	time.Sleep(1 * time.Second)
-	var _service_C m.Service = m.Service{Name: "Service-C", LastUpdated: time.Now()}
+	var _service_C m.Service = m.Service{Name: "Service-C", Host: "localHost", Port: 7444, LastUpdated: time.Now()}
+	time.Sleep(1 * time.Second)
+	var _service_B2 m.Service = m.Service{Name: "Service-B", Host: "localHost", Port: 7555, LastUpdated: time.Now()}
+	time.Sleep(1 * time.Second)
+	var _service_A2 m.Service = m.Service{Name: "Service-A", Host: "localHost", Port: 5555, LastUpdated: time.Now()}
 
-	addNewService(_service_A)
-	addNewService(_service_B)
-	addNewService(_service_C)
+	UpdateServiceInfo(_service_A)
+	UpdateServiceInfo(_service_B)
+	UpdateServiceInfo(_service_C)
+	UpdateServiceInfo(_service_A2)
+	UpdateServiceInfo(_service_B2)
 
+	printCache(t)
+	PrintCache()
+
+}
+
+// ============================================================================
+// ============== debug functions
+// ============================================================================
+func printCache(t *testing.T) {
+	for _name, _service := range ServiceCache.cache {
+		t.Logf("service name=%s  Index=%d\n", _name, _service.Index)
+		for _, _elem := range _service.List {
+			t.Logf("    %v\n", _elem)
+		}
+	}
 }
